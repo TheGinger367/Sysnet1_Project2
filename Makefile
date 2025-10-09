525955323f96ca40461b61693f01f4c35da54ff6
Parker Woodard <pbw5@students.uwf.edu>
2025-10-09 13:49:40 -0500

    Threads working, Extra Functionality such as '-nolock' and exporting not working.

--- Makefile
+++ Makefile
@@ -1,15 +1,17 @@
-#The options –g and –Wall must be enabled in the Makefile
-CFLAGS = -g -Wall 
+CFLAGS = -g -Wall -pthread
 CC = g++
 
 objects = mt-collatz.o main.o
 
 mt-collatz: $(objects)
-	$(CC) -o mt-collatz $(objects)
+	$(CC) $(CFLAGS) -o mt-collatz $(objects)
 
 main.o: main.cpp
+	$(CC) $(CFLAGS) -c main.cpp
+
 mt-collatz.o: mt-collatz.cpp
+	$(CC) $(CFLAGS) -c mt-collatz.cpp
 
 .PHONY : clean
 clean: 
-	rm mt-collatz $(objects)
+	rm -f mt-collatz $(objects)
--- main.cpp
+++ main.cpp
@@ -1,11 +1,71 @@
 #include <iostream>
 #include <string>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <chrono>
+#include <atomic>
+  
 #include "mt-collatz.hpp"
 
 using namespace std;
-int main (int argc, char *argv[]) {
 
-    collatz_a(stoi(argv[1]));
+vector<int> globalArray;
+std::atomic<int> counter(1);
+mutex mtx;
+
+void worker(int maxNumber) {
+    while (true) {
+        int n = counter.fetch_add(1);
+        if (n > maxNumber) break;
+        collatz_a(n);
+    }
+}
+
+void addToVector(int value, int index) {
+    lock_guard<mutex> lock(mtx);
+    if (index >= 0 && index < (int)globalArray.size()) {
+        globalArray[index] = value;
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    int maxNumber = stoi(argv[1]);
+    int numberThreads = stoi(argv[2]);
+
+    globalArray.resize(maxNumber + 1, 0);
+    
+    vector<thread> threads;
+
+    auto start = chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < numberThreads; i++)
+        threads.emplace_back(worker, maxNumber);
+
+    for (auto &t : threads) t.join();
+
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+    // Printing statistics
+    int maxSteps = 0;
+    long long totalSteps = 0;
+    int count = 0;
+
+    for (int i = 1; i <= maxNumber; ++i) {
+        int steps = globalArray[i];
+        totalSteps += steps;
+        if (steps > maxSteps) maxSteps = steps;
+        count++;
+    }
+
+    double avgSteps = static_cast<double>(totalSteps) / count;
+
+    cout << "Processed " << count << " numbers up to " << maxNumber << endl;
+    cout << "Max stopping time: " << maxSteps << endl;
+    cout << "Average stopping time: " << avgSteps << endl;
+    cout << "Total time: " << duration << " ms" << endl;
 
     return 0;
 }
