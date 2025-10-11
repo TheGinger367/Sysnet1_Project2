1db0bf1dfa5e9a4743b0a78c22dd8a75274ddba8
Tyler Daw <tylerdaw87@outlook.com>
2025-10-11 15:56:31 -0500

    Test Branch init

--- main.cpp
+++ main.cpp
@@ -17,15 +17,16 @@ mutex mtx;
 void worker(int maxNumber) {
     while (true) {
         int n = counter.fetch_add(1);
-        if (n > maxNumber) break;
         collatz_a(n);
+        if (n > maxNumber) break;
+        
     }
 }
 
 void addToVector(int value, int index) {
     lock_guard<mutex> lock(mtx);
     if (index >= 0 && index < (int)globalArray.size()) {
-        globalArray[index] = value;
+        globalArray[value] = globalArray[value] + 1;
     }
 }
 
@@ -38,15 +39,20 @@ int main(int argc, char *argv[]) {
     
     vector<thread> threads;
 
-    auto start = chrono::high_resolution_clock::now();
+    //auto start = chrono::high_resolution_clock::now();
+    timespec start_ts{};
+    clock_gettime(CLOCK_REALTIME, &start_ts);
 
     for (int i = 0; i < numberThreads; i++)
         threads.emplace_back(worker, maxNumber);
 
     for (auto &t : threads) t.join();
 
-    auto end = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    timespec end_ts{};
+    clock_gettime(CLOCK_REALTIME, &end_ts);
+    //auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    double duration = (end_ts.tv_sec - start_ts.tv_sec)
+                    + (end_ts.tv_nsec - start_ts.tv_nsec) / 1e9;
 
     // Printing statistics
     int maxSteps = 0;
@@ -61,11 +67,13 @@ int main(int argc, char *argv[]) {
     }
 
     double avgSteps = static_cast<double>(totalSteps) / count;
-
+    for(int i = 0; i < maxNumber; i++){
+        cout << i << "," << globalArray[i] << endl;
+    }
     cout << "Processed " << count << " numbers up to " << maxNumber << endl;
     cout << "Max stopping time: " << maxSteps << endl;
     cout << "Average stopping time: " << avgSteps << endl;
     cout << "Total time: " << duration << " ms" << endl;
-
+    cerr << maxNumber << "," << numberThreads << "," << duration << endl;
     return 0;
 }
